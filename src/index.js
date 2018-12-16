657adf30b9cac0febd146edceb942b2f70aa7aa7
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-16 22:22:59 +0100

    First challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import { message } from './myModule';
+import myCurrentLocation, { message, name, getGreeting } from './myModule';
+import subs, { add } from './math';
 
 console.log(message);
+console.log(name);
+console.log(myCurrentLocation);
+console.log(getGreeting('Jessica'));
+
+// 1. Create a new file called math.js
+// 2. Define add function
+// 3. Define sbstract
+// 4. add default
+// 5. substract named export
+// 6. Import both in index.js
+// 7. Use both to print the result
+
+console.log(add(5, 4), subs(12, 40));
--- src/myModule.js
+++ src/myModule.js
@@ -1,4 +1,11 @@
-const message = 'Some message from myModule.js';
+// Named export - Has a name. Have as many as needed
+// Default export (no name, only one)
 
-export { message };
+const message = 'Some message from myModule.js';
+const name = 'Alberto';
+const location = 'Santiago de Compostela';
+
+const getGreeting = (personName) => `Welcome to the course ${personName}`;
+
+export { message, name, location as default, getGreeting };
 
