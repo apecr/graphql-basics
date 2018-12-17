5415b7d4f963cbc2f5bc1159a50388df0c4dfd5b
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-17 01:06:58 +0100

    New user type and me query

--- src/index.js
+++ src/index.js
@@ -1,42 +1,29 @@
-import {GraphQLServer} from 'graphql-yoga';
+import { GraphQLServer } from 'graphql-yoga';
 
-// Types:
-// A. Scalar Types:
-//  1. String
-//  2. Boolean
-//  3. Int
-//  4. Float
-//  5. ID
-
-// Challenge:
-
-// Create query definition and resolver for each
-//
-// title - string product name
-// price - number as float
-// releaseYear - number as int (optional)
-// rating - float (optional)
-// inStock - boolean
 
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        title: String!
-        price: Float!
-        releaseYear: Int
-        rating: Float
-        isStock: Boolean
+      me: User!
+    }
+
+    type User {
+      id: ID!
+      name: String!
+      email: String!
+      age: Int
     }
 `;
 
 // Application Resolvers for API
 const resolvers = {
   Query: {
-    title: ()=> 'Node.js examples',
-    price: () => 213.45,
-    releaseYear: () => 1990,
-    rating: () => 9.32,
-    isStock: () => true
+    me: _ => ({
+      id: '1234abdcs',
+      name: 'Alberto Eyo',
+      email: 'albert@example.com',
+      age: 45
+    })
   }
 };
 
