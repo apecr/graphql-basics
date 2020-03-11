27005ef44514287f73e066cf32250f6a9230b925
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-11 18:58:50 +0100

    feat()
    35. Subscription Basics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import * as database from './db'
 import Query from './resolvers/Query'
 import Comment from './resolvers/Comment'
 import User from './resolvers/User'
 import Post from './resolvers/Post'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -14,10 +16,12 @@ const server = new GraphQLServer({
     Comment,
     User,
     Post,
-    Mutation
+    Mutation,
+    Subscription
   },
   context: {
-    'db': database.db
+    'db': database.db,
+    pubsub
   }
 })
 
